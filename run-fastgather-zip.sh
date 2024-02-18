44021a2bc1f02745eb1f22c69d544910226f3143
C. Titus Brown <titus@idyll.org>
2024-02-18 06:20:01 -0800

    upd, add results, clean up

--- run-fastgather-zip.sh
+++ run-fastgather-zip.sh
@@ -1,1 +1,2 @@
-/usr/bin/time -v sourmash scripts fastgather SRR606249.trim.k31.sig.zip /group/ctbrowngrp/sourmash-db/gtdb-rs214/gtdb-rs214-k31.zip -o SRR606249.x.gtdb-rs214.fastgather.zip.csv >& run-fastgather-zip.bench.txt
+#! /bin/bash
+/usr/bin/time -v sourmash scripts fastgather SRR606249.trim.k31.sig.zip gtdb-rs214-k31.zip -o SRR606249.x.gtdb-rs214.fastgather.zip.csv >& run-fastgather-zip.bench.txt
